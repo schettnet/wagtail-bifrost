19e57b851f6547ee2558899e76f444e4735b497b
schettn <nicoschett@icloud.com>
2021-03-31 23:59:00 +0200

    Improve bifrost fiels
    - Querying a bifrost file by its ubfn is added.
    - Now the secure_url of the file is returned for bifrost_files.

--- bifrost/files/schema.py
+++ bifrost/files/schema.py
@@ -5,10 +5,16 @@ from ..files.models import BifrostFile
 
 
 class Query(graphene.ObjectType):
+    bifrost_file = graphene.Field(graphene.String, ubfn=graphene.String(required=True))
     bifrost_files = graphene.List(
         graphene.String, token=graphene.String(required=False)
     )
 
     @superuser_required
+    def resolve_bifrost_file(root, info, ubfn, **kwargs):
+        file = BifrostFile.objects.get(ubfn=ubfn)
+        return file.secure_url
+
+    @superuser_required
     def resolve_bifrost_files(root, info, **kwargs):
-        return [file.get_download_url() for file in BifrostFile.objects.all()]
+        return [file.secure_url for file in BifrostFile.objects.all()]
