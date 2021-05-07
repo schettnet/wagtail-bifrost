9a4c297771e6adffbf329bb88cdb73f3ec1b9888
Nico Schett <nicoschett@icloud.com>
2021-05-07 22:27:35 +0200

    Auth: Add email authentication backend
    Using this backend authenticating with email and username is supported.

---
+++ bifrost/auth/backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+
+
+class EmailBackend:
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is not None:
+            UserModel = get_user_model()
+
+            try:
+                user = UserModel.objects.get(email__iexact=username)
+            except UserModel.DoesNotExist:
+                """Not found, try another backend"""
+            else:
+                if user.check_password(password) and user.is_active:
+                    return user
+        return None
